68283a7d51dcb405f8bf16742f64a90afc3da623
Sunimal Malkaklulage <sskmal@gmail.com>
2022-12-08 04:33:04 +0900

    trot

--- src/screen/home/Home.tsx
+++ src/screen/home/Home.tsx
@@ -1,24 +1,25 @@
 import SearchBar from "../../components/SearchBar";
 import Header from "../../components/Header";
 import CustomTable from "../../components/CustomTable";
-import { useEffect, useState } from "react";
-import {throttle} from "../../utils";
+import {useCallback, useEffect, useState} from "react";
+// import {throttle} from "../../utils";
+import {throttle}  from "lodash";
 
 const BASE_URL = "https://api.github.com"
 
 const Home = () => {
   const [repositories, setRepositories] = useState([]);
-  const [text, setText] = useState("");
+  const [text, setText] = useState("react");
   const [totalPages, setTotalPages] = useState(10);
+  // const [shouldWait,setShouldWait] = useState<boolean>(false);
 
-
-  const getRepositories = async (searchText: string,currentPage:number = 1) => {
+  const getRepositories:any = async (searchText: string,currentPage:number = 1) => {
     try {
       const response = await fetch(
         `${BASE_URL}/search/repositories?q=${searchText}&per_page=10&page=${currentPage}`
       );
       const data = await response.json();
-      console.log("result count",data.total_count);
+      console.log("search text :",searchText,"result count",data.total_count);
       setTotalPages(Math.ceil(data.total_count/10));
       setRepositories(data.items);
     } catch (error) {}
@@ -26,8 +27,11 @@ const Home = () => {
 
   const onChangeText = (text: string) => {
     setText(text);
-    throttle(getRepositories(text),3000);
+    throttledAttempts();
+
   }
+
+  const throttledAttempts =  throttle(() =>{getRepositories(text)}, 5000);
 
   return (
     <>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,31 @@
-const throttle = (cb:any, delay: number) => {
-  let flag = true;
 
-  if(flag){
-    return cb;
-    flag = false;
-    setTimeout(() => flag = true, delay);
+const throttle = (cb:(...args: any) => void, delay: number) => {
+
+  let waitingArgs: any;
+  let shouldWait:boolean = false;
+
+  const timeOutFunc = () =>{
+    if(waitingArgs == null){
+        shouldWait =false;
+    }
+    else{
+      cb(...waitingArgs);
+      waitingArgs  = null;
+      setTimeout(timeOutFunc,delay)
+    }
+  }
+
+  return (...args:any) =>{
+
+      if(shouldWait) {
+          waitingArgs = args
+          return
+      }
+      cb(...args);
+      shouldWait =true;
+      setTimeout(timeOutFunc,delay)
   }
 }
 
 export {throttle}
+
